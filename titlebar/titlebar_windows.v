51eb09ea556b441c8f7407851355264e6b74da4b
Admin <2762713521@qq.com>
2025-06-21 02:09:19 +0800

    add window dark title bar

--- titlebar/titlebar_windows.v
+++ titlebar/titlebar_windows.v
@@ -9,6 +9,6 @@ fn C.gui_prefer_dark_titlebar(voidptr, bool)
 
 pub fn prefer_dark_titlebar(handle voidptr, dark bool) { // only windows
 	$if windows {
-		C.mui_prefer_dark_titlebar(handle, dark)
+		C.gui_prefer_dark_titlebar(handle, dark)
 	}
 }
