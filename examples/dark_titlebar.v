04ca3baeee4200bee0801f1bdfabc94d9af8090a
Admin <2762713521@qq.com>
2025-06-21 19:40:52 +0800

    add window dark title bar

--- examples/dark_titlebar.v
+++ examples/dark_titlebar.v
@@ -9,9 +9,7 @@ fn main() {
 		width:   500
 		height:  300
 		on_init: fn (mut w gui.Window) {
-			$if windows {
-				titlebar.prefer_dark_titlebar(true)
-			}
+			titlebar.prefer_dark_titlebar(true)
 		}
 	)
 	window.set_theme(gui.theme_dark_bordered)
