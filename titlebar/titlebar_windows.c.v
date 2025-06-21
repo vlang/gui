b4b57dd08cab151ac5126210bd1abf1c2ebdaa23
Admin <2762713521@qq.com>
2025-06-21 19:45:03 +0800

    add window dark title bar

--- titlebar/titlebar_windows.c.v
+++ titlebar/titlebar_windows.c.v
@@ -6,7 +6,7 @@ import sokol.sapp
 
 const DWMWA_USE_IMMERSIVE_DARK_MODE = 20
 
-// set_dark_titlebar set the window titlebar to be dark or light
+// set_dark_titlebar set the window titlebar to be dark or light, api is from dwmapi.h, windows 10+ only
 pub fn set_dark_titlebar( dark bool) {
 	C.DwmSetWindowAttribute(sapp.win32_get_hwnd(),DWMWA_USE_IMMERSIVE_DARK_MODE, &dark, C.sizeof(dark))
 }
