0b49e43a993448c9f34273ef68fc5e4d2baa317b
Admin <2762713521@qq.com>
2025-06-21 10:06:30 +0800

    add window dark title bar

--- examples/dark_titlebar.v
+++ examples/dark_titlebar.v
@@ -1,6 +1,9 @@
 import gui
 import gui.titlebar
 
+// set Windows window bar as dark color
+// =============================
+
 @[heap]
 struct DropFilesApp {
 pub mut:
@@ -9,48 +12,15 @@ pub mut:
 
 fn main() {
 	mut window := gui.window(
-		state:    &DropFilesApp{}
-		width:    500
-		height:   300
-		on_init:  fn (mut w gui.Window) {
-			w.update_view(main_view)
+		state:   &DropFilesApp{}
+		width:   500
+		height:  300
+		on_init: fn (mut w gui.Window) {
 			$if windows {
 				titlebar.prefer_dark_titlebar(nil, true) // todo get handle from window
 			}
 		}
-		on_event: on_event_handler
 	)
 	window.set_theme(gui.theme_dark_bordered)
 	window.run()
-}
-
-fn main_view(window &gui.Window) gui.View {
-	w, h := window.window_size()
-	app := window.state[DropFilesApp]()
-
-	mut content := []gui.View{}
-	content << gui.text(
-		text:       'Drop Files on this Window'
-		text_style: gui.theme().b1
-	)
-
-	for df in app.dropped_files {
-		content << gui.text(text: df)
-	}
-
-	return gui.column(
-		width:   w
-		height:  h
-		sizing:  gui.fixed_fixed
-		h_align: .center
-		v_align: .middle
-		content: content
-	)
-}
-
-fn on_event_handler(mut e gui.Event, mut w gui.Window) {
-	if e.typ == .files_dropped {
-		mut app := w.state[DropFilesApp]()
-		app.dropped_files = w.get_dropped_file_paths()
-	}
 }
