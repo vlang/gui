6e58f280e55da006fab8f5f29e0f65f325f8805f
Admin <2762713521@qq.com>
2025-06-21 19:31:33 +0800

    add window dark title bar

--- titlebar/titlebar_windows.c.v
+++ titlebar/titlebar_windows.c.v
@@ -6,17 +6,7 @@ module titlebar
 const DWMWA_USE_IMMERSIVE_DARK_MODE = 20
 
 pub fn prefer_dark_titlebar(handle voidptr, dark bool) {
-	C.DwmSetWindowAttribute(handle,DWMWA_USE_IMMERSIVE_DARK_MODE, &dark, C.sizeof(C.BOOL))//need sizeof dsrk?
+	C.DwmSetWindowAttribute(handle,DWMWA_USE_IMMERSIVE_DARK_MODE, &dark, C.sizeof(C.BOOL))//need sizeof dark?
 }
 
-//
-// #include "@DIR/titlebar_windows.h"
-// #flag windows -ldwmapi
-//
-// fn C.gui_prefer_dark_titlebar(voidptr, bool)
-//
-// pub fn prefer_dark_titlebar(handle voidptr, dark bool) {
-// 	C.gui_prefer_dark_titlebar(handle, dark)
-// }
-//
 
