64e854787b1b2f77698888a57d33d41908428e30
Admin <2762713521@qq.com>
2025-06-21 20:35:01 +0800

    add window dark title bar

--- titlebar/titlebar_windows.c.v
+++ titlebar/titlebar_windows.c.v
@@ -10,6 +10,5 @@ const dwmwa_use_immersive_dark_mode = 20
 
 // set_dark_mode set the window titlebar to be dark or light, api is from dwmapi.h, windows 10+ only
 pub fn set_dark_mode(dark bool) {
-	C.DwmSetWindowAttribute(C.HWND(sapp.win32_get_hwnd()), dwmwa_use_immersive_dark_mode,
-		&dark, sizeof(dark))
+	C.DwmSetWindowAttribute(C.HWND(sapp.win32_get_hwnd()), 20, &dark, sizeof(dark))
 }
